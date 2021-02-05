8e5a3dbde645a3892f4ad02a56b0a5b3af8162b3
Gabriel <grmpeixoto@gmail.com>
2021-02-04 21:49:02 -0300

    "update 0.0.2"

--- scrapping.py
+++ scrapping.py
@@ -1,23 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = 'https://coinmarketcap.com/currencies/bitcoin/markets/' # $BTC value
+url_ = 'https://valor.globo.com/valor-data/' # Price of Dolar in real 
 
-url = 'https://coinmarketcap.com/currencies/bitcoin/markets/'
-    
-def normalize(url):
+def normalize(link):
 
-    r = requests.get(url)
+    r = requests.get(link)
     soup = BeautifulSoup(r.content, "html.parser")
     value = soup.find("div", {"class":"priceValue___11gHJ"})
-
     value_ = value.contents[0]
-
     value_ = value_.replace('$', "")
     value_ = value_.replace(',', "")
-
     newvalue_ = float(value_)
 
     return newvalue_
 
 
-print(normalize(url))
+def normalize_dolar(link):
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content, "html.parser")
+    value = soup.find("div", {"class":"cell auto data-cotacao__ticker_quote"})
+    value_ = value.contents[0]
+    value_ = value_.replace(',', '.')
+    newvalue = float(value_)
+
+    return newvalue 
+
+print("valor do #BTC:: $", normalize(url))
+print("Valor do dolar :: R$", normalize_dolar(url_))
